54ea16a9d15ca9c15a32c11b536d48d39ce07ae1
romesful <ilya.romesful@gmail.com>
2021-10-19 22:14:15 +0500

    Описаны и определены некоторые методы модуля лексического анализатора

--- SimpleCompiler_FGnT/SimpleCompiler_FGnT/LexicalAnalyzer.cpp
+++ SimpleCompiler_FGnT/SimpleCompiler_FGnT/LexicalAnalyzer.cpp
@@ -1,2 +1,19 @@
 #include "LexicalAnalyzer.h"
 
+LexicalAnalyzer::LexicalAnalyzer(const string& filename_input, const string& filename_output)
+{
+	io = IO_Module(filename_input, filename_output);
+}
+
+void LexicalAnalyzer::parse_tokens()
+{
+	char c = ' ';
+	do
+	{
+		switch (c)
+		{
+			case ' ': break;
+		}
+		c = io.get_next_char();
+	} while (c != EOF);
+}
--- SimpleCompiler_FGnT/SimpleCompiler_FGnT/LexicalAnalyzer.h
+++ SimpleCompiler_FGnT/SimpleCompiler_FGnT/LexicalAnalyzer.h
@@ -5,7 +5,9 @@
 
 class LexicalAnalyzer {
 public:
-
+	IO_Module io;
+	LexicalAnalyzer(const string& filename_input, const string& filename_output);
+	void parse_tokens();
 private:
 	vector<Token> tokens;
 };
